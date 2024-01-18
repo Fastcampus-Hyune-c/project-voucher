532367cc36bbaf0382e99098c0928b6e746050e0
hyune-c <choi8608@gmail.com>
2024-01-18 19:49:05 +0900

    [프로젝트 세팅] employee 영속화 구현 2: 영속화 로직을 storage 레이어로 이동

--- src/main/java/com/example/projectvoucher/domain/employee/EmployeeService.java
+++ src/main/java/com/example/projectvoucher/domain/employee/EmployeeService.java
@@ -1,25 +1,30 @@
 package com.example.projectvoucher.domain.employee;
 
 import com.example.projectvoucher.app.controller.response.EmployeeResponse;
+import com.example.projectvoucher.storage.employee.EmployeeEntity;
+import com.example.projectvoucher.storage.employee.EmployeeRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class EmployeeService {
-    private final Map<Long, EmployeeResponse> employeeResponseMap = new HashMap<>();
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     // 사원 생성
     public Long create(final String name, final String position, final String department) {
-        Long no = employeeResponseMap.size() + 1L;
-        employeeResponseMap.put(no, new EmployeeResponse(no, name, position, department));
+        final EmployeeEntity employeeEntity = employeeRepository.save(new EmployeeEntity(name, position, department));
 
-        return no;
+        return employeeEntity.id();
     }
 
     // 사원 조회
     public EmployeeResponse get(final Long no) {
-        return employeeResponseMap.get(no);
+        final EmployeeEntity employeeEntity = employeeRepository.findById(no)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+
+        return new EmployeeResponse(employeeEntity.id(), employeeEntity.name(), employeeEntity.position(), employeeEntity.department());
     }
 }

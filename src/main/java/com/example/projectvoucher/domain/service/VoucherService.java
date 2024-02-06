db4d01db72ef639b3e2013e4c240b736bd4b4bf2
hyune-c <choi8608@gmail.com>
2024-02-06 19:52:46 +0900

    [기본 구현 2차] 리팩토링: RequestContext 구현

--- src/main/java/com/example/projectvoucher/domain/service/VoucherService.java
+++ src/main/java/com/example/projectvoucher/domain/service/VoucherService.java
@@ -1,6 +1,6 @@
 package com.example.projectvoucher.domain.service;
 
-import com.example.projectvoucher.common.type.RequesterType;
+import com.example.projectvoucher.common.dto.RequestContext;
 import com.example.projectvoucher.common.type.VoucherAmountType;
 import com.example.projectvoucher.common.type.VoucherStatusType;
 import com.example.projectvoucher.storage.voucher.VoucherEntity;
@@ -48,7 +48,7 @@ public class VoucherService {
 
     // 상품권 발행
     @Transactional
-    public String publishV2(final RequesterType requesterType, final String requesterId, final LocalDate validFrom, final LocalDate validTo, final VoucherAmountType amount) {
+    public String publishV2(final RequestContext requestContext, final LocalDate validFrom, final LocalDate validTo, final VoucherAmountType amount) {
         final String code = UUID.randomUUID().toString().toUpperCase().replaceAll("-", "");
         final VoucherEntity voucherEntity = new VoucherEntity(code, VoucherStatusType.PUBLISH, validFrom, validTo, amount);
 
@@ -57,7 +57,7 @@ public class VoucherService {
 
     // 상품권 사용 불가 처리
     @Transactional
-    public void disableV2(final RequesterType requesterType, final String requestId, final String code) {
+    public void disableV2(final RequestContext requestContext, final String code) {
         final VoucherEntity voucherEntity = voucherRepository.findByCode(code)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상품권입니다."));
 
@@ -66,7 +66,7 @@ public class VoucherService {
 
     // 상품권 사용
     @Transactional
-    public void useV2(final RequesterType requesterType, final String requestId, final String code) {
+    public void useV2(final RequestContext requestContext, final String code) {
         final VoucherEntity voucherEntity = voucherRepository.findByCode(code)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상품권입니다."));
 
